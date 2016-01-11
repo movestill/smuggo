a21110f87f9bc4fe304b436db08145088efeb140
Tim Gion <movestill@gmail.com>
2016-01-10 23:20:06 -0500

    Added builds of Windows and Linux executables.

--- core/build.sh
+++ core/build.sh
@@ -9,7 +9,19 @@ case "$1" in
         echo 'Testing smuggo.'
         go test $SOURCE_FILES $TEST_FILES
         ;;
-    -*)
+    win32)
+        echo 'Building Windows x86 smuggo_win32.exe.'
+        env GOOS=windows GOARCH=386 go build -o smuggo_win32.exe $SOURCE_FILES
+        ;;
+    win64)
+        echo 'Building Windows x64 smuggo_x64.exe.'
+        env GOOS=windows GOARCH=amd64 go build -o smuggo_x64.exe $SOURCE_FILES
+        ;;
+    linux64)
+        echo 'Building Linux x64 smuggo_linux64.exe.'
+        env GOOS=linux GOARCH=amd64 go build -o smuggo_linux64 $SOURCE_FILES
+        ;;
+    *)
         echo 'Building smuggo.'
         go build -o smuggo $SOURCE_FILES
         ;;
